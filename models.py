61a172bd0b153b6da02aa4236365c5d2ffe1493f
istalif <bread998@naver.com>
2019-12-18 19:14:21 +0900

    이번에도 안되면 손절함

--- models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Friend(models.Model):
-	image = models.ImageField(upload_to='images/')
-	character = models.CharField(max_length=300)
-	
--- views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from .models import Friend
-# Create your views here.
-
-
-def home(request): 
-	friends = Friend.objects
-	return render(request, 'home.html', {'friends':friends})
